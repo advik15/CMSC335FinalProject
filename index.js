0fb12b80bba50433b9ae9fde1c937358eae0351a
Advik Sachdeva <adviks15@gmail.com>
2023-12-05 14:15:55 -0500

    added search functionality and displaying books

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+
+
 process.stdin.setEncoding("utf8");
 
 
-
 const bodyParser = require("body-parser"); /* To handle post parameters */
-
 
 
 const http = require('http');
@@ -27,11 +27,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/bookreq", async (req, res) =>{
-
   const title = req.query.title
-    const fetch = require('node-fetch');
+  const fetch = (await import('node-fetch')).default; // Dynamic import for node-fetch
 
-    const url = `https://hapi-books.p.rapidapi.com/search/{req.body.searchtext}`;
+
+    const url = `https://hapi-books.p.rapidapi.com/search/${req.query.searchtext}`;
     const options = {
       method: 'GET',
       headers: {
@@ -42,8 +42,25 @@ app.get("/bookreq", async (req, res) =>{
     
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
         console.log(result);
+
+        let output = "<form action='/add-to-library' method='post'>";
+        
+        result.forEach((prop, index) => {
+            const bookIdentifier = encodeURIComponent(`${prop.name}-${prop.authors[0]}-${prop.cover}`);
+
+            output += `
+                <div>
+                    <h2>${prop.name}</h2><br>
+                    <p>${prop.authors[0]}</p><br>
+                    <img src='${prop.cover}'><br>
+                    Add to Library <input type='checkbox' name='selectedBook' value='${bodyIdentifier}'>
+                </div>
+            `;
+        });
+        output += "<button type='submit'>Submit</button></form>";
+        res.render("searchoutput", {output:output});
     } catch (error) {
         console.error(error);
     }
